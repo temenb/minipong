72b820d643649a63757279b1ada217821e911a3b
Oleksii Temchenko <OTemchenko@fxcess.com>
2025-09-11 15:13:51 +0300

    unstable

--- lib/entity/game.dart
+++ lib/entity/game.dart
@@ -1,24 +1,23 @@
 class Game {
   final String id;
-  final List<String> characteracterIds;
+  List<String> characterIds;
   final DateTime createdAt;
 
   Game({
     required this.id,
-    required this.characteracterIds,
+    required this.characterIds,
     required this.createdAt,
   });
 
   Map<String, dynamic> toJson() => {
     'id': id,
-    'characteracterIds': characteracterIds,
+    'characterIds': characterIds,
     'createdAt': createdAt.toIso8601String(),
   };
 
   static Game fromJson(Map<String, dynamic> json) => Game(
     id: json['id'],
-    characteracterIds: List<String>.from(json['characteracterIds']),
+    characterIds: List<String>.from(json['characterIds']),
     createdAt: DateTime.parse(json['createdAt']),
   );
 }
-
--- lib/managers/game_manager.dart
+++ lib/managers/game_manager.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/foundation.dart';
-import 'package:minipong/entity/match.dart';
+import 'package:minipong/entity/game.dart';
 import 'package:minipong/entity/character.dart';
 import 'package:minipong/repositories/match_repository.dart';
 import 'package:minipong/repositories/character_repository.dart';
 
 class GameManager extends ChangeNotifier {
-  List<Match> get matches => MatchRepository.instance.matches;
-  List<Character> get characters => CharacterRepository.instance.characters;
-
-  Match? _currentMatch;
-  final List<Character> _selectedCharacters = [];
-  final Set<String> activePlayerIds = {};
+  Game? _currentGame;
 
   GameManager() {
     init();
   }
 
   void reset() {
-    _currentMatch = null;
-    _selectedCharacters.clear();
-    notifyListeners();
+    _currentGame = Game(
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      characterIds: [],
+      createdAt: DateTime.now(),
+    );
+    // _selectedCharacters.clear();
   }
 
   /// Сброс состояния игры
@@ -31,47 +29,41 @@ class GameManager extends ChangeNotifier {
       print('=====================================================================================================================');
       print('GameManager.init: error: $e\n$s');
     }
-    notifyListeners();
   }
 
-  Match? get currentMatch => _currentMatch;
-  List<Character> get selectedCharacters => List.unmodifiable(_selectedCharacters);
+  Game? get currentGame => _currentGame;
 
-  /// Создание нового матча с выбранными персонажами
-  void createNewMatch(List<Character> characters) {
-    final newMatch = Match(
-      id: DateTime.now().millisecondsSinceEpoch.toString(),
-      battleIds: [],
-      createdAt: DateTime.now(),
-    );
-    _currentMatch = newMatch;
-    _selectedCharacters
-      ..clear()
-      ..addAll(characters);
-    MatchRepository.instance.addMatch(newMatch);
-    notifyListeners();
-  }
+/// Создание нового матча с выбранными персонажами
+// void createNewMatch(List<Character> characters) {
+//     id: DateTime.now().millisecondsSinceEpoch.toString(),
+//     battleIds: [],
+//     createdAt: DateTime.now(),
+//   );
+//   _selectedCharacters
+//     ..clear()
+//     ..addAll(characters);
+//   MatchRepository.instance.addMatch(newMatch);
+// }
 
-  bool isPlayerActive(String id) => activePlayerIds.contains(id);
-
-  void setPlayerActive(String id, bool active) {
-    if (active) {
-      activePlayerIds.add(id);
-    } else {
-      activePlayerIds.remove(id);
-    }
-    notifyListeners();
-  }
-
-  void addPlayer(Character character) {
-    CharacterRepository.instance.addCharacter(character);
-    activePlayerIds.add(character.id);
-    notifyListeners();
-  }
-
-  void removePlayer(String id) {
-    CharacterRepository.instance.removeById(id);
-    activePlayerIds.remove(id);
-    notifyListeners();
-  }
+// void createNewGame() {
+//   _currentGame = Game(
+//     id: DateTime.now().millisecondsSinceEpoch.toString(),
+//     characterIds: activePlayerIds.toList(),
+//     createdAt: DateTime.now(),
+//   );
+// }
+//
+// void setPlayerActive(String id, bool active) {
+// }
+//
+// void addPlayer(Character character) {
+//   CharacterRepository.instance.addCharacter(character);
+//   activePlayerIds.add(character.id);
+//   updateCurrentGameCharacterIds();
+//
+// void removePlayer(String id) {
+//   CharacterRepository.instance.removeById(id);
+//   activePlayerIds.remove(id);
+//   updateCurrentGameCharacterIds();
+// }
 }
