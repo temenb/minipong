aa7dded74f2bf78b10dd3c88665e938c3f2d49c1
Oleksii Temchenko <oleksii.temchenko@gmail.com>
2025-12-20 19:16:31 +0200

    it works

--- services/front/lib/main.dart
+++ services/front/lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // генерируется автоматически через flutterfire CLI
+import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello, Flutter + Firebase!'),
+        ),
+      ),
+    );
+  }
 }
